09ca846b190bb429b161261237df4d42db4d103d
AmrrElShazlyy <98602938+AmrrElShazlyy@users.noreply.github.com>
2022-05-03 20:16:33 +0200

    git test

---
+++ app/src/main/java/com/example/myweatherapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myweatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.e("TAG", "onCreate: git test ", )
+    }
+}
