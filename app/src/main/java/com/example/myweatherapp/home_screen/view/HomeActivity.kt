343c145d00ab995ba0c6945c56e46551c1c67df6
AmrrElShazlyy <98602938+AmrrElShazlyy@users.noreply.github.com>
2022-05-06 21:29:32 +0200

    finished home screen UI

--- app/src/main/java/com/example/myweatherapp/home_screen/view/HomeActivity.kt
+++ app/src/main/java/com/example/myweatherapp/home_screen/view/HomeActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
@@ -19,6 +21,15 @@ class HomeActivity : AppCompatActivity() {
 
     lateinit var toggle: ActionBarDrawerToggle
 
+    lateinit var locationTv : TextView
+    lateinit var dateTv : TextView
+    lateinit var currentIconIv : ImageView
+    lateinit var currentDescrTv : TextView
+    lateinit var currentPressureTv : TextView
+    lateinit var currentHumidityTv : TextView
+    lateinit var currentCloudsTv : TextView
+    lateinit var currentWindSpeedTv : TextView
+
     lateinit var hourlyRecyclerView: RecyclerView
     lateinit var hourlyAdapter: HourlyAdapter
     lateinit var hourlyLayoutManager: LinearLayoutManager
@@ -32,10 +43,23 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+
+        initUI()
         initNavDrawer()
         initHourlyRecyclerView()
         initDailyRecyclerView()
 
+    }
+
+    fun initUI(){
+        locationTv = findViewById(R.id.homeTextViewLocation)
+        dateTv =  findViewById(R.id.homeTextViewDate)
+        currentIconIv = findViewById(R.id.homeImageViewCurrentIcon)
+        currentDescrTv = findViewById(R.id.homeTextViewCurrentDescr)
+        currentPressureTv = findViewById(R.id.homeTextViewCurrentPressure)
+        currentHumidityTv = findViewById(R.id.homeTextViewCurrentHumidity)
+        currentCloudsTv = findViewById(R.id.homeTextViewCurrentClouds)
+        currentWindSpeedTv = findViewById(R.id.homeTextViewCurrentWindSpeed)
     }
 
     fun initNavDrawer(){
