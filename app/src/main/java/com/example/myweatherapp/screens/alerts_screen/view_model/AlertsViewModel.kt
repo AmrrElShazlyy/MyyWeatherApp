ec6c1d154536a31594c30b2bf00857e487442c54
AmrrElShazlyy <98602938+AmrrElShazlyy@users.noreply.github.com>
2022-05-19 03:23:38 +0200

    ...

--- app/src/main/java/com/example/myweatherapp/screens/alerts_screen/view_model/AlertsViewModel.kt
+++ app/src/main/java/com/example/myweatherapp/screens/alerts_screen/view_model/AlertsViewModel.kt
@@ -35,37 +35,3 @@ class AlertsViewModel(private val repo: RepoInterface) : ViewModel() {
 
 }
 
-
-
-
-/*
- private var _weatherData : MutableLiveData<WeatherDataModel> = MutableLiveData<WeatherDataModel>()
-    var weatherData : LiveData<WeatherDataModel> = _weatherData
-
-    fun getWeatherDataModelFromNetwork(lat : String , lon :String , units : String , language : String){
-        viewModelScope.launch(Dispatchers.IO) {
-
-            //var lat = SharedPrefrencesHandler.getSettingsFromSharedPref(Constants.LAT_KEY,"laaat",)
-            //val sharedPreferences = getApplication.getSharedPreferences("settings_shared_pref", Context.MODE_PRIVATE)
-
-            _weatherData.postValue(repo.fetchWeatherDataFromNetwork(lat.toDouble(),lon.toDouble(),units,language,"minutely"))
-            //_weatherData.postValue(repo.fetchWeatherDataFromNetwork(33.44,94.0,"metric","ar","minutely,hourly"))
-            weatherData = _weatherData
-        }
-    }
-
-    fun insertWeatherDataModelIntoDB(weatherDataModel: WeatherDataModel){
-        viewModelScope.launch(Dispatchers.IO) {
-            repo.insertWeatherData(weatherDataModel)
-
-        }
-    }
-
-
-    fun insertAlert( lat : Double , lon : Double , startDate : Long , endDate : Long , alertDays : List<String>,
-                      alertTime : Long, alertType : String) {
-        viewModelScope.launch(Dispatchers.IO) {
-            try {
-                var alertlocal = AlertLocal(null,lat, lon, startDate, endDate, alertDays, alertTime, alertType)
-                repo.insertAlertLocal(alertlocal)
- */
